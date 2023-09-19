0ede7d3c936c8ad207c77ad6126744e74d9b23ac
Abhijeet Gupta <57822136+abhix201@users.noreply.github.com>
2023-09-19 11:40:07 +0530

    Time: 19 ms (89.52%), Space: 21.6 MB (51.89%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int size = nums.size(), sum = 0, ans = 0, temp = 0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<size;i++){
+            if(nums[i] == nums[i+1]){
+                temp = nums[i];
+                break;
+            }
+        }
+        for(int i=0;i<size;i++){
+            sum += nums[i];
+        }
+        ans = (size * (size+1)/2);
+        ans = ans - sum;
+        vector<int> finale{temp,ans+temp};
+        return finale;
+    }
+};
