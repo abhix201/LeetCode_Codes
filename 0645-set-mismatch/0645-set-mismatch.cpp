b1af3de696fe7a565247de473307f2881d7bfab9
Abhijeet Gupta <57822136+abhix201@users.noreply.github.com>
2023-09-19 11:41:02 +0530

    Time: 31 ms (50.24%), Space: 21.6 MB (55.09%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -12,8 +12,7 @@ public:
         for(int i=0;i<size;i++){
             sum += nums[i];
         }
-        ans = (size * (size+1)/2);
-        ans = ans - sum;
+        ans = (size * (size+1)/2) - sum;
         vector<int> finale{temp,ans+temp};
         return finale;
     }
