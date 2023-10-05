a54881c0401efeeba27ba9fe3ec5ce4be6014b63
Abhijeet Gupta <57822136+abhix201@users.noreply.github.com>
2023-10-05 12:13:32 +0530

    Time: 57 ms (76.66%), Space: 39 MB (35.76%) - LeetHub

---
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        int mini = INT_MAX, maxi = INT_MIN;
+        int size = nums.size();
+        map<int,int> mp;
+        for(int i=0;i<size;i++){
+            if(nums[i] % 2 == 0){
+                mp[nums[i]]++;
+            }
+        }
+        int maxEven = -1;
+        int maxFreq = 0;
+        for(auto s : mp){
+            if(s.second > maxFreq){
+                maxFreq = s.second;
+                maxEven = s.first;
+            }
+            else if(s.second == maxFreq){
+                maxEven = min(maxEven, s.first);
+            }
+        }
+        return maxEven;
+    }
+};
