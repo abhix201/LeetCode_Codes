8bc81a9d0226339a2d21d292efd2d67d4d960eb5
Abhijeet Gupta <abhix201@gmail.com>
2024-09-15 10:52:55 +0530

    Time: 34 ms (91.92%), Space: 33.7 MB (95.92%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        int size = words.size(); //size of the array
-        bool a[26] = {};
-        for(char c : allowed) a[c - 'a'] = true;
-        //populating a where we find which characters are present in allowed
-        for(string w : words){
-            for(char c : w){
-                if(!a[c-'a']){
+        //more optimized solution
+        int size = words.size();
+        bool a[123] = {};
+        for(char c:allowed) a[c] = true;
+        for(string w: words){
+            for(char c:w){
+                if(!a[c]){
                     size--;
-                    break;//checking each character in w to check if there is any mismatch character in the w. If present then the loop breaks
+                    break;
                 }
             }
-        }
+        }   
         return size;
     }
 };
