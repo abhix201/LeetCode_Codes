6eb7138fc7694aafbf2d4888d887a71d07637945
Abhijeet Gupta <abhix201@gmail.com>
2025-01-16 22:44:44 +0530

    Time: 3 ms (57.65%), Space: 9.6 MB (69.47%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> f(26, 0);
+        for (char ch : s) {
+            f[ch - 'a']++;
+        }
+        for (char ch : t) {
+            f[ch - 'a']--;
+        }
+        for (int i : f) {
+            if (i != 0) return false;
+        }
+        return true;
+    }
+};
