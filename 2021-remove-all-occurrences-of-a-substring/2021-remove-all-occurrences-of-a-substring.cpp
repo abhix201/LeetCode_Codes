1827fc6598cb918510f03100e849b72ca1231422
Abhijeet Gupta <abhix201@gmail.com>
2025-02-11 10:21:56 +0530

    Time: 4 ms (15.12%), Space: 12.1 MB (8.29%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        string ans;
+        int m = part.size();
+
+        for (char c : s) {
+            ans.push_back(c); 
+            if (ans.size() >= m && ans.substr(ans.size() - m, m) == part) {
+                ans.erase(ans.size() - m, m);
+            }
+        }
+        return ans;
+    }
+};
