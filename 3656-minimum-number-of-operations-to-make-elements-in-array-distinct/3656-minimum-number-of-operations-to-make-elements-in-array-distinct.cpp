c5e0188a414ec9b2382696cd5732c867ac1fa26f
Abhijeet Gupta <abhix201@gmail.com>
2025-04-08 22:49:03 +0530

    Time: 0 ms (100%), Space: 27.4 MB (90.61%) - LeetHub

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        vector<int> f(101);
+        for(int i = nums.size() - 1; i >= 0; --i){
+            if(++f[nums[i]] > 1)
+            return (i + 3) / 3;
+        }
+        return 0;
+    }
+};
