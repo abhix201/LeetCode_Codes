925691fe27422fd5083178cda737715eb6ba78c7
Abhijeet Sanjeevkumar Gupta <abhix201@gmail.com>
2025-09-15 10:25:17 +0530

    Time: 0 ms (100%), Space: 9.4 MB (23.59%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
-        int cnt = 0, wCount = 0;
+        // Use a set for O(1) average time lookups (fast)
+        unordered_set<char> broken_set(brokenLetters.begin(), brokenLetters.end());
+        
         stringstream ss(text);
         string word;
+        int typable_words = 0;
+        
+        // Process word by word to save memory (low space)
         while (ss >> word) {
-            wCount++;
+            bool is_broken = false;
             for (char c : word) {
-                if (isValid(c, brokenLetters)) {
-                    cnt++;
+                if (broken_set.count(c)) { // O(1) check
+                    is_broken = true;
                     break;
                 }
             }
+            if (!is_broken) {
+                typable_words++;
+            }
         }
-        return wCount - cnt;
-    }
-private:
-    bool isValid(char c, string brokenLetters) {
-        return string(brokenLetters).find(c) != string::npos;
+        
+        return typable_words;
     }
 };
