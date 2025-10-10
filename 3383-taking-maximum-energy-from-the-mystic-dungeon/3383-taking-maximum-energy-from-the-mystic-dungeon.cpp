958494ceb4325ca1ca5a8ad058f5df74ccd23499
Abhijeet Sanjeevkumar Gupta <abhix201@gmail.com>
2025-10-10 22:52:00 +0530

    Time: 123 ms (92.25%), Space: 145.2 MB (95.07%) - LeetHub

---
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        int n = energy.size();
+        int ans = INT_MIN;
+        for (int i = n - k; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j >= 0; j -= k) {
+                sum += energy[j];
+                ans = max(ans, sum);
+            }
+        }
+        return ans;
+    }
+};
